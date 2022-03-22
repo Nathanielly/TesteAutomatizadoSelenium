3ecc06e920d2fdfa06e67073a1cd10a8190f3bf2
Nathanielly <nathanielly23@hotmail.com>
2022-03-22 13:51:25 -0300

    Teste automatizado atualização

---
+++ Alura.LeilaoOnline.Selenium/PageObjects/LoginPageObject.cs
@@ -0,0 +1,51 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.LeilaoOnline.Selenium.PageObjects
+{
+    public class LoginPageObject
+    {
+
+        private IWebDriver driver;
+
+        //localizadores
+        private By byInputLogin;
+        private By byInputSenha;
+        private By byBotaoLogin;
+
+        public LoginPageObject(IWebDriver driver)
+        {
+            this.driver = driver;
+
+            //inicialização dos localizadores
+            byInputLogin = By.Id("Login");
+            byInputSenha = By.Id("Password");
+            byBotaoLogin = By.Id("btnLogin");
+
+        }
+        public void Visitar()
+        {
+            driver.Navigate().GoToUrl("http://localhost:5000/Autenticacao/Login");
+
+        }
+
+        public void PreencherFormulario(string login, string senha)
+        {
+            driver.FindElement(byInputLogin).SendKeys(login);
+            driver.FindElement(byInputSenha).SendKeys(senha);
+        }
+
+        public void SubmeterFormulario()
+        {
+            driver.FindElement(byBotaoLogin).Submit(); //submit vai fazer a mesma função do click.
+        }
+
+        
+
+
+    }
+}
---
+++ Alura.LeilaoOnline.Selenium/Testes/AoEfetuarLogin.cs
@@ -0,0 +1,61 @@
+﻿using Alura.LeilaoOnline.Selenium.Fixtures;
+using Alura.LeilaoOnline.Selenium.PageObjects;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Alura.LeilaoOnline.Selenium.Testes
+{
+    [Collection("Chrome Driver")]
+    public class AoEfetuarLogin
+    {
+        private IWebDriver driver;
+
+        public AoEfetuarLogin(TestFixture fixture)
+        {
+            driver = fixture.Driver;
+        }
+
+        [Fact]
+        public void DadoCredenciaisValidasDeveIrParaPaginaDeDashboard()
+        {
+
+            //Arrange - 
+            var loginPo = new LoginPageObject(driver);
+            loginPo.Visitar();
+            loginPo.PreencherFormulario("fulano@example.org", "123");
+
+            //Act - 
+            loginPo.SubmeterFormulario();
+
+            //Assert - 
+            Assert.Contains("Dashboard", driver.Title);
+
+
+        }
+
+        [Fact]
+        public void DadoCredenciaisInvalidasDeveContinuarLogin()
+        {
+
+            //Arrange - 
+            var loginPo = new LoginPageObject(driver);
+            loginPo.Visitar();
+            loginPo.PreencherFormulario("fulano@example.org", "");
+
+            //Act - 
+            loginPo.SubmeterFormulario();
+
+            //Assert - 
+            Assert.Contains("Login", driver.PageSource);
+
+
+        }
+
+
+    }
+}
